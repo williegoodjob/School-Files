d365b2afa87531e88b9b85a4b51e90f6ca7bdc39
willie <willie@goodjob.idv.tw>
2023-07-11 22:58:59 +0800

    刪除多餘註解

--- web/phptest/db.php
+++ web/phptest/db.php
@@ -9,12 +9,11 @@
         <?php
         $servername = "localhost";
         $username = "root";
-        $password = "123456";
+        $password = "*********";
         $dbname = "DB";
 
-        // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
+
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
@@ -29,7 +28,6 @@
         $result = $conn->query($sql);
         
         if ($result->num_rows > 0) {
-            // output data of each row
             echo "<tr>" . "<td>time</td>" . "<td>account</td>" . "<td>text</td>" . "</tr>";
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>" . "<td>" . $row["time"] . "</td>" . "<td>" . $row["account"] . "</td>" . "<td>" . $row["text"] . "</td>" . "</tr>";
@@ -47,11 +45,10 @@
         <input type="submit" value="送出">
     </form>
 
-    <!-- all user -->
     <form action="#" method="post">
         <input type="submit" value="all user">
     </form>
-    <!-- search username -->
+    
     <form action="#" method="post">
         <label>search:<input type="text" name="account"></label><br />
         <input type="submit" value="送出">
--- web/phptest/insert.php
+++ web/phptest/insert.php
@@ -1,21 +1,19 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "123456";
+$password = "********";
 $dbname = "DB";
 
 $account = $_POST["account"];
 $text = $_POST["text"];
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "INSERT INTO chat (account, text)" . "VALUES ('$account', '$text')";
-// $sql = "SELECT * FROM chat";
 $result = $conn->query($sql);
 
 $conn->close();
