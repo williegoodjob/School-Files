48821a3704ba9c9e739a29405d5075f7c4ff2928
willie <willie@goodjob.idv.tw>
2023-10-23 16:44:56 +0800

    6(2)

---
+++ C_C++/6/(2)/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    for (int i = 2; i <= 40; i += 2)
+    {
+        printf("%d, ", i);
+    }
+    printf("\n");
+    system("PAUSE");
+    return 0;
+}
