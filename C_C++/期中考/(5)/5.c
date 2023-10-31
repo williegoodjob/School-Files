2be884a9d831f0f5da96ff40cb2aac3a76e44a81
willie <willie@goodjob.idv.tw>
2023-10-31 19:11:53 +0800

    簡化/修正程式邏輯

--- C_C++/期中考/(5)/5.c
+++ C_C++/期中考/(5)/5.c
@@ -8,7 +8,7 @@ int main()
         int x, s1 = 0, s2 = 1;
         printf("請輸入第一次想要計算的階層個數:");
         scanf("%d", &x);
-        if (x > 0)
+        if (x >= 0)
         {
             for (int i = 1; i <= x; i++)
             {
@@ -19,20 +19,12 @@ int main()
                 }
                 s1 += s2;
             }
-        }
-        else if (x < 0)
-        {
-            printf("%d是負數所以沒有階乘計算結果!\n", x);
+            printf("%d的階乘計算結果為:%d\n", x, s2);
+            printf("%d的階乘總和結果為:%d\n\n", x, s1);
         }
         else
         {
-            s2 = 1;
-            s1 = 0;
-        }
-        if (x >= 0)
-        {
-            printf("%d的階乘計算結果為:%d\n", x, s2);
-            printf("%d的階乘總和結果為:%d\n\n", x, s1);
+            printf("%d是負數所以沒有階乘計算結果!\n", x);
         }
     }
     system("PAUSE");
