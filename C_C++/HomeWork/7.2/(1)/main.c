dc5ed3d529806ccf5c1d7dffc7228198d6a6b3aa
willie <willie@goodjob.idv.tw>
2023-12-11 16:12:33 +0800

    Add code for generating random characters

---
+++ C_C++/HomeWork/7.2/(1)/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+    char name[50];
+    printf("輸入你的英文姓名:");
+    scanf("%s", name);
+    printf("\n");
+    if (strcmp(name, "Xuser") == 0)
+    {
+        srand(time(0)); // for generating random numbers
+        printf("產生1000個字元元素之字元陣列如下:\n");
+        for (int i = 0; i < 1000; i++)
+        {
+            printf("name[%d]=%c\n", i + 1, 'A' + rand() % 26); // Generates a random character from A-Z
+        }
+    }
+    else
+    {
+        printf("姓名驗證錯誤 >>> 強制登出!\n");
+    }
+    return 0;
+}
