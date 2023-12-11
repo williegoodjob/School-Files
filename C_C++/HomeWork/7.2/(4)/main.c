cdb82d2c91581db881d34785a37c459ae8d836cd
willie <willie@goodjob.idv.tw>
2023-12-11 16:27:21 +0800

    Update main.c file with modified variable name and print statement

--- C_C++/HomeWork/7.2/(4)/main.c
+++ C_C++/HomeWork/7.2/(4)/main.c
@@ -4,23 +4,16 @@
 int main(int argc, char *argv[]) 
 {
 	int a[10]={0};		
-	int i=0, max;		
-	printf("-----尋找最大值-----\n");
+	int i=0, mid;		
+	printf("-----尋找中間值-----\n");
 	printf("請連續輸入十個數值-\n");
 	for(i=0;i<10;i++)		
 	{
 	   printf("請輸入第 %d 個資料：", i+1 );
 	   scanf("%d", &a[i]);  
 	}
-	max=a[0];			
-	for(i=0;i<10;i++)		
-	{
-	  if (max<a[i])	
-	  {
-		  max=a[i];
-	  }
-	}
-	printf("最大值：%d\n\n",max); 
+	mid=a[4];
+	printf("中間值：%d\n\n",mid); 
 	
 	system("PAUSE");	
 	return 0;
