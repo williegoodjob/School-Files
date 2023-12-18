98429b0f225f086e1d9b67800fe431d13eea4bfc
willie <willie@goodjob.idv.tw>
2023-12-18 17:08:01 +0800

    Add code for printing array elements and calculating array size

---
+++ C_C++/HomeWork/7.3/(1)/main.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main(){
+    int a[] = {73, 168, 349, 229, 543};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+    printf(">>> 整數陣列a中的每個元素各佔4個Bytes <<<\n");
+    int length = sizeof(a) / sizeof(a[0]);
+    printf("=== 整數陣列a記憶體空間總共佔%d個Bytes ===\n", length * 4);
+
+    char b[] = {'B', 'O', 'O', 'K', 'S'};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("b[%d] = %c\n", i, b[i]);
+    }
+    printf(">>> 英文陣列a中的每個元素各佔4個Bytes <<<\n");
+    int length1 = sizeof(b) / sizeof(b[0]);
+    printf("=== 英文陣列a記憶體空間總共佔%d個Bytes ===\n", length1 * 4);
+
+    char* c[] = {"蔡", "宇", "崴"};
+    for (int i = 0; i < 3; i++)
+    {
+        //printf("c[%d] = %c\n", i, c[i]);
+        printf("c[%d] = %s\n", i, c[i]);
+    }
+    printf(">>> 整數陣列a中的每個元素各佔4個Bytes <<<\n");
+    int length2 = sizeof(b) / sizeof(b[0]);
+    printf("=== 整數陣列a記憶體空間總共佔%d個Bytes ===\n", length2 * 4);
+    
+}
