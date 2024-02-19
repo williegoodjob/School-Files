f5588309b812f9e3d750367fb947df1552fc2bd7
willie <willie@goodjob.idv.tw>
2024-02-19 16:32:21 +0800

    update

--- Cpp/24-02-19/second/main.cpp
+++ Cpp/24-02-19/second/main.cpp
@@ -7,6 +7,6 @@ int main()
     int n;
     cout << "請輸入一個整數:";
     cin >> n;
-    cout << "n的開根號是" << setiosflags(ios::fixed) << setprecision(3) << sqrt(n) << endl;
-    cout << "n的3次方是" <<setiosflags(ios::fixed) << setprecision(0) << pow(n, 3) << endl;
+    cout << n << "的開根號是" << setiosflags(ios::fixed) << setprecision(3) << sqrt(n) << endl;
+    cout << n << "的3次方是" << setiosflags(ios::fixed) << setprecision(0) << pow(n, 3) << endl;
 }
