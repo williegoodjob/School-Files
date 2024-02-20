7a16a71b29c332ff068ee769812b66ecab374ba1
willie <willie@goodjob.idv.tw>
2024-02-20 11:30:44 +0800

    second

---
+++ Cpp/24-02-20/second/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    float c, d;
+
+    a = 10.0 / 2;
+    b = 10.0 / 3;
+    c = 10.0 / 2;
+    d = 10.0 / 3;
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+    cout << "c=" << c << endl;
+    cout << "d=" << d << endl;
+}
