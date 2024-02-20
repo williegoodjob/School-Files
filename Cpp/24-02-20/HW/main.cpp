ea50a2de4c41f40a8630ad904ff53d1c14557e1e
willie <willie@goodjob.idv.tw>
2024-02-20 11:38:00 +0800

    home work

---
+++ Cpp/24-02-20/HW/main.cpp
@@ -0,0 +1,21 @@
+// 輸入abc三個數 解出ax*x+bx+c=0的解
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    float x1, x2;
+    cout << "輸入a,b,c三個數" << endl;
+    cout << "a=" << endl;
+    cin >> a;
+    cout << "b=" << endl;
+    cin >> b;
+    cout << "c=" << endl;
+    cin >> c;
+    x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+    x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+    cout << "x1=" << x1 << endl;
+    cout << "x2=" << x2 << endl;
+    return 0;
+}
