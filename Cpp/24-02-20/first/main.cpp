394b09fbbefefe09a9074741a8a0937caac44229
willie <willie@goodjob.idv.tw>
2024-02-20 11:20:16 +0800

    first

---
+++ Cpp/24-02-20/first/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    float b;
+    double c;
+    int d;
+    a = 123.456789;
+    b = 123.456789;
+    c = 123.456789;
+    d=sizeof(a);
+    d=sizeof(b);
+    d=sizeof(c);
+    return 0;
+}
