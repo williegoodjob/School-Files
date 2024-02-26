0da3d2d634a898982c60e324edf98be27bb3a8f0
willie <willie@goodjob.idv.tw>
2024-02-26 14:16:22 +0800

    float to int

--- Cpp/24-02-26/BMI/main.cpp
+++ Cpp/24-02-26/BMI/main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 int main()
 {
-    float height, weight, BMI;
+    int height, weight;
+    float BMI;
     cout << "請輸入身高(cm):";
     cin >> height;
     cout << "請輸入體重(kg):";
     cin >> weight;
-    height /= 100;
-    BMI = weight / (height * height);
+    float Hm = height / 100.0;
+    BMI = weight / (Hm * Hm);
     cout << setiosflags(ios::fixed) << setprecision(1);
     cout << "BMI = " << BMI << endl;
     return 0;
