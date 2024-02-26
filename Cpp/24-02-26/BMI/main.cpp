1164cc0a4c9958bf955d57cbe6f2264bee6d2dd8
willie <willie@goodjob.idv.tw>
2024-02-26 15:04:43 +0800

    HW

--- Cpp/24-02-26/BMI/main.cpp
+++ Cpp/24-02-26/BMI/main.cpp
@@ -7,16 +7,16 @@ int main()
     int weight, height;
     float w, h;
     float BMI;
-    cout << "本程式會自動計算BMI值" << endl;
-    cout << "請輸入你的體重(kg):";
-    cin >> weight;
-    cout << "請輸入你的身高(cm):";
+    cout << "BMI計算器" << endl;
+    cout << "請輸入身高(cm): ";
     cin >> height;
+    cout << "請輸入體重(kg):";
+    cin >> weight;
     w = (float)weight;
     h = (float)height;
     h = h / 100;
     BMI = w / pow(h, 2);
-    cout << setiosflags(ios::fixed) << setprecision(1);
+    cout << setiosflags(ios::fixed) << setprecision(2);
     cout << "BMI=" << BMI << endl;
     return 0;
 }
