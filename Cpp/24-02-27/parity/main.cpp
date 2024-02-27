29aad6eafb677d7ec11e76958cf2f3257189707e
willie <willie@goodjob.idv.tw>
2024-02-27 11:46:53 +0800

    新增奇偶數判斷程式

---
+++ Cpp/24-02-27/parity/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 块计 耞案计
+    int n;
+    cout << "叫块俱计:";
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << n << "琌案计";
+    }
+    else
+    {
+        cout << n << "琌计";
+    }
+    return 0;
+}
