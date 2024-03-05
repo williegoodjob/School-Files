dd26c6071b7aa0b391071409cfb58f13e0488822
willie <willie@goodjob.idv.tw>
2024-03-05 13:01:38 +0800

    新增和修改了兩個 C++ 檔案

---
+++ Cpp/24-03-05/HW/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "請輸入一個整數:";
+    cin >> size;
+    cout << setiosflags(ios::right);
+    for (int i = -size; i < size; i++)
+    {
+        int count = size - abs(i);
+        cout << setw(abs(i) + size);
+        for (int j = 0; j < count; j++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+    return 0;
+}
--- Cpp/24-03-05/first/main.cpp
+++ Cpp/24-03-05/first/main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 int main()
 {
-    for (int i = 0; i < 4; i++) 
+    for (int i = 1; i < 8; i++)
     {
-        for (int j = 4; j > i; j--) 
+        if (i <= 4)
         {
-            cout << "*";
+            for (int j = 0; j < i; j++)
+            {
+                cout << "*";
+            }
+        }
+        else
+        {
+            for (int j = 4; j > i - 4; j--)
+            {
+                cout << "*";
+            }
         }
         cout << endl;
     }
