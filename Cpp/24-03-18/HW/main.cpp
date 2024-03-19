bd26dabfd986edb7dbf02a5ad83fd2993c610283
willie <willie@goodjob.idv.tw>
2024-03-19 10:18:31 +0800

    更新程式碼以計算年齡並將結果寫入age.txt檔案

--- Cpp/24-03-18/HW/main.cpp
+++ Cpp/24-03-18/HW/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
 
 using namespace std;
 
@@ -8,12 +9,16 @@ int main()
     ofstream fout;
     fout.open("age.txt");
     string name;
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
     int year;
     cout << "請問您的姓名:";
     cin >> name;
     cout << "請問您出生哪年(民國):";
     cin >> year;
-    fout << name << "出生民國" << year << "年，今年" << 113 - year << "歲。";
+    fout << name << "出生民國" << year ;
+    year += 1911;
+    fout << "年，今年" << 1900 + ltm->tm_year - year << "歲。";
     fout.close();
     cout << "檔案已經輸出到age.txt" << endl;
     return 0;
