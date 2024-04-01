2c2a008d7dbe4174916ab354bca50f144b9540be
willie <willie@goodjob.idv.tw>
2024-04-01 14:36:08 +0800

    移除 AQI 資料檔案並新增 CSV 讀取程式

---
+++ Cpp/24-04-01/csv/main.cpp
@@ -0,0 +1,41 @@
+// ´M§ä¼ê´òªº¸ê®Æ
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    string line;
+    vector<string> data;
+    fin.open("data.csv");
+    if (!fin)
+    {
+        cout << "ÀÉ®×¶}±Ò¥¢±Ñ" << endl;
+        return 1;
+    }
+    while (getline(fin, line))
+    {
+        data.push_back(line);
+    }
+    fin.close();
+
+    for (int i = 0; i < data.size(); i++)
+    {
+        cout << data[i] << endl;
+    }
+    cout << "---------------------------------" << endl;
+    // ´M§ä¼ê´ò
+    for (int i = 0; i < data.size(); i++)
+    {
+        if (data[i].find("¼ê´ò") != string::npos)
+        {
+            cout << data[i] << endl;
+        }
+    }
+    return 0;
+}
---
+++ Cpp/24-04-01/csv/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+
+    for (int i = 0; i < data.size(); i++)
+    {
+        cout << data[i] << endl;
+    }
