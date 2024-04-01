a3d3e5a12641896787fdf515049fd3e2e987a120
willie <willie@goodjob.idv.tw>
2024-04-01 14:27:39 +0800

    修正空白行

--- Cpp/24-04-01/first/main.cpp
+++ Cpp/24-04-01/first/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     string str = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20";
-    // ¤À³Î¦r¦ê¥H³r¸¹
+    
     istringstream iss(str);
     string token;
     while (getline(iss, token, ','))
