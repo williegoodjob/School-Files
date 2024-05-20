5746bc6117dec73b3429895d367612ff1ab93450
willie <willie@goodjob.idv.tw>
2024-05-20 16:31:04 +0800

    chore: Refactor score class and operator overloads for addition

--- Cpp/24-05-20/HW/main.cpp
+++ Cpp/24-05-20/HW/main.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    score peter(70, 65), mary, tom;
+ score peter(70,65),mary,tom;
     ifstream in("test.dat");
     ofstream out("final.dat");
-    cin >> mary;
-    cout << mary;
-    in >> tom;
-    cout << tom << endl;
-    cout << peter + tom + mary;
+    cin>>mary;
+    cout<<mary;
+    in>>tom;
+    cout<<tom<<endl;
+    out<<peter+tom+mary;
+    in.close();
+    out.close();
     return 0;
 }
