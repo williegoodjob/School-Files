eb1ab18107d39073a588de4af5bbfc4b3e4792fe
willie <willie@goodjob.idv.tw>
2024-05-20 16:19:00 +0800

    HW V2

--- Cpp/24-05-20/HW/main.cpp
+++ Cpp/24-05-20/HW/main.cpp
@@ -6,14 +6,13 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    score peter(70, 65), mary, tom;
     ifstream in("test.dat");
     ofstream out("final.dat");
-    score peter(80, 70), mary, tom;
     cin >> mary;
+    cout << mary;
     in >> tom;
-    cout << peter;
-    mary = peter + 30;
-    cout << "mary=" << mary << endl;
-    out << peter + tom + mary;
+    cout << tom << endl;
+    cout << peter + tom + mary;
     return 0;
 }
