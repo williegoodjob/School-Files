4a9dd986b52873509dc355030213091fd37ce67f
willie <willie@goodjob.idv.tw>
2024-11-09 13:48:30 +0800

    重構 main.cpp，簡化 setup 函數並新增 loop 函數以處理串口輸入

--- Arduino/school_prj/src/main.cpp
+++ Arduino/school_prj/src/main.cpp
@@ -1,46 +1,19 @@
 #include <Arduino.h>
 
-char a, b;
-int c, d;
-char e[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-long f, g;
-int h[2][2];
-char s[6] = "Hello";
-
-// variables
-void setup()
-{
-    a = 55;
-    b = 82;
-    c = a * b;
-    d = (a - b) * 100;
-    h[0][0] = e[0];
-    h[0][1] = e[2];
-    h[1][0] = e[4];
-    h[1][1] = e[6];
-    f = 12345 * a;
-    g = 87654 * b;
-
-    Serial.begin(9600);
-
-    // 列出變數名稱、記憶體位址和數值
-    Serial.println("變數名稱\t記憶體位址\t數值");
-    Serial.print("a\t\t"); Serial.print((int)&a, HEX); Serial.print("\t\t"); Serial.println((int)a);
-    Serial.print("b\t\t"); Serial.print((int)&b, HEX); Serial.print("\t\t"); Serial.println((int)b);
-    Serial.print("c\t\t"); Serial.print((int)&c, HEX); Serial.print("\t\t"); Serial.println(c);
-    Serial.print("d\t\t"); Serial.print((int)&d, HEX); Serial.print("\t\t"); Serial.println(d);
-    for (int i = 0; i < 8; i++) {
-        Serial.print("e["); Serial.print(i); Serial.print("]\t\t"); Serial.print((int)&e[i], HEX); Serial.print("\t\t"); Serial.println((int)e[i]);
-    }
-    Serial.print("f\t\t"); Serial.print((int)&f, HEX); Serial.print("\t\t"); Serial.println(f);
-    Serial.print("g\t\t"); Serial.print((int)&g, HEX); Serial.print("\t\t"); Serial.println(g);
-    Serial.print("h[0][0]\t\t"); Serial.print((int)&h[0][0], HEX); Serial.print("\t\t"); Serial.println(h[0][0]);
-    Serial.print("h[0][1]\t\t"); Serial.print((int)&h[0][1], HEX); Serial.print("\t\t"); Serial.println(h[0][1]);
-    Serial.print("h[1][0]\t\t"); Serial.print((int)&h[1][0], HEX); Serial.print("\t\t"); Serial.println(h[1][0]);
-    Serial.print("h[1][1]\t\t"); Serial.print((int)&h[1][1], HEX); Serial.print("\t\t"); Serial.println(h[1][1]);
-    Serial.print("s\t\t"); Serial.print((int)&s, HEX); Serial.print("\t\t"); Serial.println(s);
+void setup() {
+  Serial.begin(115200);
 }
 
-void loop()
-{
+void loop() {
+  char buffer[5]; // 用來存放讀取的字元
+  while (Serial.available() < 4); // 等待至少有4個字元可讀取
+  Serial.readBytes(buffer, 4); // 一次讀取4個字元
+  buffer[4] = '\0'; // 確保字串結尾
+
+  if (strcmp(buffer, "ABCD") == 0) {
+    Serial.write(0xAB);
+    Serial.write(0xCD);
+    delay(1000);
+    Serial.println("ABCD");
+  }
 }
